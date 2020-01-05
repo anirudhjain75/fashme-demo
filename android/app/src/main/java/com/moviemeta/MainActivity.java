5c41d2045337a7a94b57d523adb40d4cb316274e
Anirudh Jain <anirudh.jain@outlook.com>
2020-01-05 16:14:37 +0530

    rename app from temp to MovieMeta

--- android/app/src/main/java/com/moviemeta/MainActivity.java
+++ android/app/src/main/java/com/moviemeta/MainActivity.java
@@ -1,4 +1,4 @@
-package com.temp;
+package com.moviemeta;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "temp";
+    return "MovieMeta";
   }
 }
--- android/app/src/main/java/com/moviemeta/MainApplication.java
+++ android/app/src/main/java/com/moviemeta/MainApplication.java
@@ -1,4 +1,4 @@
-package com.temp;
+package com.moviemeta;
 
 import android.app.Application;
 import android.content.Context;
